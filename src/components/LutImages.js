92323fdb29c6fab8d39837bce8ad807e6ba00b78
Junweon Park <merlin@jellybus.com>
2020-04-20 21:53:04 +0900

    Added ample images and LUTs

--- src/components/LutImages.js
+++ src/components/LutImages.js
@@ -16,46 +16,56 @@ import _JOYFUL5_ from "./luts/JOYFUL-5.png";
 import _MELLOW_ from "./luts/MELLOW.png";
 import _METRO1_ from "./luts/METRO-1.png";
 import _METRO2_ from "./luts/METRO-2.png";
-import _MILKY_WAY_ from "./luts/MILKY_WAY.png";
+//import _MILKY_WAY_ from "./luts/MILKY_WAY.png"; // 너무 밝아서 제외
 import _NEON1_ from "./luts/NEON-1.png";
 import _NEON2_ from "./luts/NEON-2.png";
 import _NEON3_ from "./luts/NEON-3.png";
-import _NEON4_ from "./luts/NEON-4.png";
-import _NEON5_ from "./luts/NEON-5.png";
+import _STARRY_NIGHT_ from "./luts/NEON-4.png";
+import _MILKY_WAY_ from "./luts/NEON-5.png";
 import _NEON6_ from "./luts/NEON-6.png";
 import _OBSESSION_ from "./luts/OBSESSION.png";
 import _PATAGONIA_ from "./luts/PATAGONIA.png";
 import _SECRET_ from "./luts/SECRET.png";
+import _BOLD_1A_ from "./luts/BOLD-1.png";
+import _BOLD_1B_ from "./luts/BOLD-1-2.png";
+import _BOLD_2_ from "./luts/BOLD-2.png";
+import _BOLD_3_ from "./luts/BOLD-3.png";
+import _SIMPLE_BW_ from "./luts/SIMPLE_BW.jpg";
 
 const LutImages = [
+  { name: "JOYFUL-1", url: _JOYFUL1_ },
+  { name: "JOYFUL-2", url: _JOYFUL4_ },
+  { name: "BREEZE", url: _BREEZE_ },
+  { name: "BREAD", url: _BREAD_ },
+  { name: "ISLAND", url: _ISLAND_ },
+  { name: "DARLING", url: _DARLING_ },
+  { name: "FOREST", url: _FOREST_ },
   { name: "ALICE", url: _ALICE_ },
   { name: "BABYBLUE", url: _BABYBLUE_ },
-  { name: "BREAD", url: _BREAD_ },
-  { name: "BREEZE", url: _BREEZE_ },
-  { name: "BRIGHT_STANDARD", url: _BRIGHT_STANDARD_ },
-  { name: "DARLING", url: _DARLING_ },
   { name: "DUSKY", url: _DUSKY_ },
+  { name: "BRIGHT", url: _BRIGHT_STANDARD_ },
   { name: "FINE_DINING", url: _FINE_DINING_ },
-  { name: "FOREST", url: _FOREST_ },
-  { name: "ISLAND", url: _ISLAND_ },
-  { name: "JOYFUL-1", url: _JOYFUL1_ },
-  { name: "JOYFUL-2", url: _JOYFUL2_ },
-  { name: "JOYFUL-3", url: _JOYFUL3_ },
-  { name: "JOYFUL-4", url: _JOYFUL4_ },
-  { name: "JOYFUL-5", url: _JOYFUL5_ },
+  //{ name: "JOYFUL-2", url: _JOYFUL2_ },
+  //{ name: "JOYFUL-3", url: _JOYFUL3_ },
+  //{ name: "JOYFUL-5", url: _JOYFUL5_ },
   { name: "MELLOW", url: _MELLOW_ },
-  { name: "METRO-1", url: _METRO1_ },
-  { name: "METRO-2", url: _METRO2_ },
+  { name: "METRO", url: _METRO1_ },
+  { name: "METRO-MORE", url: _METRO2_ },
   { name: "MILKY_WAY", url: _MILKY_WAY_ },
-  { name: "NEON-1", url: _NEON1_ },
-  { name: "NEON-2", url: _NEON2_ },
-  { name: "NEON-3", url: _NEON3_ },
-  { name: "NEON-4", url: _NEON4_ },
-  { name: "NEON-5", url: _NEON5_ },
+  //{ name: "NEON-1", url: _NEON1_ },
+  { name: "NEON", url: _NEON2_ },
+  //{ name: "NEON-3", url: _NEON3_ },
+  { name: "STARRY NIGHT", url: _STARRY_NIGHT_ },
+  { name: "MILKY WAY", url: _MILKY_WAY_ },
   { name: "NEON-6", url: _NEON6_ },
   { name: "OBSESSION", url: _OBSESSION_ },
   { name: "PATAGONIA", url: _PATAGONIA_ },
   { name: "SECRET", url: _SECRET_ },
+  { name: "BOLD_1A", url: _BOLD_1A_ },
+  { name: "BOLD_1B", url: _BOLD_1B_ },
+  { name: "BOLD_2", url: _BOLD_2_ },
+  { name: "BOLD_3", url: _BOLD_3_ },
+  { name: "SIMPLE_BW", url: _SIMPLE_BW_ },
 ];
 
 export default LutImages;
